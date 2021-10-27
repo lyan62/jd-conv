8a7ce65b36326908c9d77609d2924467cdf6e801
Wenyan Li <wenyan.li@hotmail.com>
2021-10-27 22:57:49 +0800

    add pair pred

--- dataset/querySet.py
+++ dataset/querySet.py
@@ -59,7 +59,59 @@ class querySet(Dataset):
     def load_dict(dict_path):
         return pickle.load(open(dict_path, "rb"))
 
+class queryPairSet(Dataset):
+    def __init__(self, data_path, query1_col_name, query2_col_name, target_col_name, dict_out_prefix):
+        self.df = pd.read_csv(data_path, index_col=0)
+        self.query1_col_name = query1_col_name
+        self.query2_col_name = query2_col_name
+        self.target_col_name = target_col_name
+        self.dict_path =  "./output/"+dict_out_prefix+"_dict.pkl"
+        self.dict = self.build_dict()
 
+
+    def __getitem__(self, index):
+        cur_row = self.df.iloc[index]
+        text1 = cur_row[self.query1_col_name]
+        text2 = cur_row[self.query2_col_name]
+        label = self.dict["target"].get(str(cur_row[self.target_col_name]))
+        query = [self.dict["source"].get(c, 1) for c in text1] + [2] + [self.dict["source"].get(c, 1) for c in text2]
+        return query, label, text1+"<SEP>"+text2
+
+    def __len__(self):
+        return len(self.df)
+
+    def build_dict(self):
+        if not os.path.exists(self.dict_path):
+            dictionary = self._build_dict()
+            creat_output_dir("output")
+            pickle.dump(dictionary, open(self.dict_path, "wb"))
+            return dictionary
+        else:
+            return self.load_dict(self.dict_path)
+
+    def _build_dict(self):
+        print("building dictionary....")
+        source_freqs = Counter()
+        label_freqs = Counter()
+        for idx, row in tqdm(self.df.iterrows(), total=len(self.df)):
+            for c in row[self.query1_col_name] + row[self.query2_col_name]:
+                source_freqs[str(c)] += 1
+            label_freqs[str(row[self.target_col_name])] += 1
+
+        s_word2index = {v: i + 3 for i, (v, c) in enumerate(list(source_freqs.items()))}
+        s_word2index['<PAD>'] = 0
+        s_word2index['<UNK>'] = 1
+        s_word2index['<SEP>'] = 2
+
+        t_word2index = {v: i for i, (v, c) in enumerate(list(label_freqs.items()))}
+        data_dict = {'source': s_word2index,
+                     'target': t_word2index}
+
+        return data_dict
+
+    @staticmethod
+    def load_dict(dict_path):
+        return pickle.load(open(dict_path, "rb"))
 
 def my_collate_fn(batch):
     batch.sort(key=lambda x: len(x[0]), reverse=True)  # sort sentences in decreasing length
@@ -78,9 +130,10 @@ def my_collate_fn(batch):
 
 
 if __name__ == "__main__":
-    train_set = querySet("./data/crosswoz_chat_intent.csv",
-                         query_col_name="msg",
-                         target_col_name="intent")
+    train_set = queryPairSet("./data/crosswoz_chat_pairs/dev.csv",
+                         query1_col_name="msg1",
+                         query2_col_name="msg2",
+                         target_col_name="label")
     from torch.utils.data import Dataset, DataLoader
     data_loader = DataLoader(train_set, batch_size=3, shuffle=True, collate_fn=my_collate_fn,
                              num_workers=4, pin_memory=True, sampler=None)
